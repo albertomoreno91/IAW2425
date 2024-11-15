a7f285f238c0bcd091ba281b28fb9fe0e39d11e0
Alberto Moreno <albertomoreno91@iesamachado.org>
2024-11-16 00:56:29 +0100

    comm

--- javascript/Ejercicios JQuery/ej2.js
+++ javascript/Ejercicios JQuery/ej2.js
@@ -1,1 +1,9 @@
 /* Rellena este fichero */
+$(document).ready(function () {
+    $("#btn-ocultar").click(function () {
+        $(".pares, #encabezado").hide()
+    })
+    $("#btn-mostrar").click(function () {
+        $(".pares, #encabezado").show()
+    })
+});
