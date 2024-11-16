097d9cfc9aeaef14114afcf9320f44825f35b2b5
Alberto Moreno <albertomoreno91@iesamachado.org>
2024-11-16 01:30:20 +0100

    parrrrrrrrrse

--- javascript/Ejercicios JQuery/ej3.js
+++ javascript/Ejercicios JQuery/ej3.js
@@ -1,1 +1,18 @@
 /* Rellena este fichero */
+$(document).ready(function () {
+    $("#btn-aumentar").click(function () {
+        $(".pares, #encabezado").each(function () {
+            let tamano_actual = parseFloat($(this).css("font-size"))
+            $(this).css("font-size", (tamano_actual + 4) + "px")
+        })
+    })
+});
+
+$(document).ready(function () {
+    $("#btn-disminuir").click(function () {
+        $(".pares, #encabezado").each(function () {
+            let tamano_actual = parseFloat($(this).css("font-size"))
+            $(this).css("font-size", (tamano_actual - 4) + "px")
+        })
+    })
+});
