4ed3f79d775bbcf5e34b12abc748d7c320173871
albertomoreno91 <albertomoreno91@iesamachado.org>
2024-11-27 16:37:07 +0100

    php

---
+++ php/operadoresNumericos.php
@@ -0,0 +1,13 @@
+<?php
+$num1 = 65;
+$num2 = 7;
+echo "Suma = "; echo $num1 + $num2;
+echo "<br>";
+echo "Resta = "; echo $num1 - $num2;
+echo "<br>";
+echo "Multiplicación = "; echo $num1 * $num2;
+echo "<br>";
+echo "División = "; echo $num1 / $num2;
+echo "<br>";
+echo "Resto = "; echo $num1 % $num2;
+?>
