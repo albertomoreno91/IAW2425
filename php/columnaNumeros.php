191d39ae626667e92375ccb0b41b4ab07801eeb6
albertomoreno91 <albertomoreno91@iesamachado.org>
2024-12-02 13:49:16 +0100

    php

---
+++ php/columnaNumeros.php
@@ -0,0 +1,21 @@
+<?php
+$numero = 1;
+$total = 10;
+echo "<table border='1'>";
+while ($numero <= $total){
+    echo "<tr><td>$numero</td></tr>"
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Columna</title>
+</head>
+<body>
+    
+</body>
+</html>
