9b15adf17deb822b02e3dedca483535f8fc10340
albertomoreno91 <albertomoreno91@iesamachado.org>
2024-12-02 14:06:40 +0100

    php

---
+++ php/foreach1.php
@@ -0,0 +1,22 @@
+<?php
+$refranes = array("Más vale pájaro en mano que ciento volando.", 
+"El que tiene boca se equivoca.", "No hay mal que por bien no venga.", 
+"El hábito no hace al monje.", "El que a hierro mata, a hierro muere.");
+foreach ($refranes as $refran) {
+    echo "<ul>";
+    echo "<li>$refran</li>";
+    echo "</ul>";
+};
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For Each 1</title>
+</head>
+<body>
+    
+</body>
+</html>
