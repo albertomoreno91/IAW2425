fd70dac81326005c59aca1e0d1fa99ea03892ebe
albertomoreno91 <albertomoreno91@iesamachado.org>
2024-12-12 14:02:04 +0100

    php

--- php/login.php
+++ php/login.php
@@ -1,30 +1,30 @@
 <!DOCTYPE html>
-<html lang="es">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Autenticación</title>
 </head>
 <body>
-    <form action="login.php" method="POST">
-        <input type="text" name="nombre">
-        <input type="password" name="contrasena">
-        <input type="submit" value="login">
-    </form>
-    <?php
-    $usuario = htmlspecialchars($_POST["nombre"]);
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+    <label for="usuario">Usuario:</label><input type="text" name="usuario"><br>
+    <label for="contrasena">Contraseña</label><input type="password" name="contrasena"><br>
+    <input type="submit" value="Login">
+</form>
+
+<?php
+if ($_POST){
+    $usuario = htmlspecialchars($_POST["usuario"]);
     $contrasena = htmlspecialchars($_POST["contrasena"]);
-    if ($usuario != "" && $contrasena != "") {
-        if ($usuario == "admin" && $contrasena == "H4CK3R4$1R"){
-            echo "Enhorabuena, te has logueado";
-        }
-        else {
-            echo "Introduce las credenciales correctas";
-        }
+    if ($usuario == "admin" && $contrasena == "H4CK3R4$1R") {
+        echo "<p>Acceso concedido</p>";
     }
     else {
-        echo "Introduce algún valor en las dos cajas";
+        echo strcmp($_POST["usuario"],"") == 0 ? "<p>Debe introducir un nombre de usuario</p>" : "<p>Usuario y/o contraseña inválido</p>";
+        echo "<p>Acceso denegado</p>";
     }
-    ?>
+}
+?>
+
 </body>
+
 </html>
