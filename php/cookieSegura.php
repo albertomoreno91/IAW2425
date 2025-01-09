0b46674ffb75f474e4de7964a230b269892055d5
albertomoreno91 <albertomoreno91@iesamachado.org>
2025-01-09 14:29:33 +0100

    php

--- php/cookieSegura.php
+++ php/cookieSegura.php
@@ -1,14 +1,14 @@
 <?php
-    setcookie("nombre", "Tekito Lakaka", time() + 86400);
-    setcookie("color", "Colorao");
-    setcookie("equipo", "Racing de Miano");
+    setcookie("nombre", "Tekito Lakaka", time() + 86400, "", "", 1, 1);
+    setcookie("color", "Colorao", time() + 0, "", "", 1, 1);
+    setcookie("equipo", "Racing de Miano", time() + 0, "", "", 1, 1);
 ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cookies</title>
+    <title>Cookies seguras</title>
 </head>
 <body>
     <h1>Prueba de cookies</h1>
