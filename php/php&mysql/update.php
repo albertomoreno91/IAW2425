cb3cc1846bf6cb93cab700555d55a09791610778
albertomoreno91 <albertomoreno91@iesamachado.org>
2025-01-15 13:42:06 +0100

    php

---
+++ php/php&mysql/update.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "***";
+$username = "***";
+$password = "***";
+$dbname = "***";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "UPDATE USUARIOS SET apellidos = 'Padial Naranjo' WHERE nombre = 'Samuel'";
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?> 
